cb94120129bdfb7b6c74a086019dd55ca03a7bfe
Patrick Duvall <duvallp208@gmail.com>
2019-05-01 08:49:14 -0600

    Add rest contrtoller routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Song.destroy_all
+
+Song.create(title: "Don't Stop Believin'", length: 251, play_count: 760847)
+Song.create(title: "Don't Worry Be Happy", length: 280, play_count: 65862)
+Song.create(title: "Chicken Fried", length: 183, play_count: 521771)
+Song.create(title: "Radioactive", length: 10000, play_count: 623547)
